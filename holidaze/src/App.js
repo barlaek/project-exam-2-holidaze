e5e3951c8cea4324fabc9a249331a19302aa368b
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-20 20:53:13 +0200

    building a searchbar prototype

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -1,10 +1,11 @@
 import { ApiHook } from './components/api';
+import { Navigation } from './components/header/navigation';
 import './App.css';
 
 function App() {
   const { data, loading, error } = ApiHook("https://api.noroff.dev/api/v1/holidaze/venues");
   return <div>
-    Hello world
+    <Navigation />
   </div>
 }
 
---
+++ holidaze/src/components/header/navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SearchBar } from "./searchbar";
+
+export function Navigation() {
+    return (
+        <div>
+            <h2>Logo</h2>
+            <SearchBar />
+        </div>
+    )
+}
