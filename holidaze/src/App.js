f8e73c565423f3d08d4b0769f9e48334b204a92e
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-20 21:05:46 +0200

    search bar prototype

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -1,9 +1,7 @@
-import { ApiHook } from './components/api';
 import { Navigation } from './components/header/navigation';
 import './App.css';
 
 function App() {
-  const { data, loading, error } = ApiHook("https://api.noroff.dev/api/v1/holidaze/venues");
   return <div>
     <Navigation />
   </div>
--- holidaze/src/components/header/searchbar.js
+++ holidaze/src/components/header/searchbar.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { ApiHook } from "../api";
 
 export function SearchBar() {
+    const { data, loading, error } = ApiHook("https://api.noroff.dev/api/v1/holidaze/venues");
+
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue);
+        if(searchInput !== '') {
+            const filteredData = data.filter((item) => {
+                return Object.values(item).join('').toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
+            })
+            
+            setFilteredData(filteredData);
+        } else {
+            setFilteredData(data)
+        }
+    }
 
 
     return (
-        <input type="text" placeholder="search" />
+        <input type="text" placeholder="search" onChange={(e) => searchItems(e.target.value)} />
     )
 }
