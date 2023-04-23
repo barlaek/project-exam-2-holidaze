0bb04d9218813fb5cf3a5c39a6284b1958022c3e
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-23 16:55:22 +0200

    venue by id

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='venue' element={<Venue />} />
+          <Route path='/:id' element={<Venue />} />
         </Route>
       </Routes>
     </div>
--- holidaze/src/components/venues/venue.js
+++ holidaze/src/components/venues/venue.js
@@ -2,16 +2,22 @@ import { useParams } from "react-router-dom";
 import { ApiHook } from "../api";
 
 export function Venue() {
-    let id = useParams();
+    let { id } = useParams();
 
     const  { data, loading, error } = ApiHook(
         `https://api.noroff.dev/api/v1/holidaze/venues/${id}`);
 
+    if(loading) {
+        return <div>loading</div>
+    }
+
+    if(error) {
+        return <div>Error</div>
+    }
+
     return (
         <div>
-            {data.map((venue) => (
-                <div>{venue.name}</div>
-            ))}
+            <h2>{data.name}</h2>
         </div>
     )
 }
