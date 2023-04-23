1842fc40d1649938ceab1ca9de495a0becf1c831
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-23 17:57:12 +0200

    updating endpoints

--- holidaze/src/components/api/endpoints.js
+++ holidaze/src/components/api/endpoints.js
@@ -1,7 +1,11 @@
-export const venuesUrl = "https://api.noroff.dev/api/v1/holidaze/venues";
+export const API_BASE_URL = "https://api.noroff.dev/api/v1/holidaze"
 
-export const bookingsUrl = "https://api.noroff.dev/api/v1/holidaze/bookings";
+export const venuesUrl = `${API_BASE_URL}/venues`;
 
-export const profilesUrl = "https://api.noroff.dev/api/v1/holidaze/profiles";
+export const bookingsUrl = `${API_BASE_URL}/bookings`;
 
-export const authUrl = "https://api.noroff.dev/api/v1/holidaze/auth";
+export const profilesUrl = `${API_BASE_URL}/profiles`;
+
+export const registerUrl = `${API_BASE_URL}/auth/register`;
+
+export const loginUrl = `${API_BASE_URL}/auth/login`;
