e0f0c6a5c3fc98f1166f647d738a5bf15e7140e6
Liridona <91525128+barlaek@users.noreply.github.com>
2023-05-14 17:10:22 +0200

    logout component shell

--- holidaze/src/components/authentication/logout.js
+++ holidaze/src/components/authentication/logout.js
@@ -1,2 +1,10 @@
 // Removes the JWT from storage, invalidating the HTTP request
 // And changes the context state to false.
+
+export function Logout() {
+    fuf
+
+    return (
+        <button>Log out</button>
+    )
+}
--- holidaze/src/components/header/logBtn.js
+++ holidaze/src/components/header/logBtn.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
+import { Logout } from "../authentication/logout"
 
 export function LogBtn() {
     return (
         <div>
             <Link to="/login"><button>Click me</button></Link>
+            <Logout />
         </div>
     )
 }
