a4b6714ef5994808e1de51ca0729e26c905ac041
Liridona <91525128+barlaek@users.noreply.github.com>
2023-05-15 17:55:44 +0200

    context folder

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -5,18 +5,21 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Venue } from './components/venues/venue';
 import { Registration } from './components/authentication/registerUser';
+import UserContext from './components/context/context';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/:id' element={<Venue />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Registration />}/>
-        </Route>
-      </Routes>
+      <UserContext>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/:id' element={<Venue />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Registration />}/>
+          </Route>
+        </Routes>
+      </UserContext>
     </div>
   )
 }
--- holidaze/src/components/context/context.js
+++ holidaze/src/components/context/context.js
@@ -1,1 +1,21 @@
 // Create a context state for users logged in
+
+import { createContext, useContext, useState } from "react";
+
+const User = createContext(null);
+
+const UserContext = ({ children }) => {
+    const [ isLoggedIn, setIsLoggedIn ] = useState();
+
+    return (
+        <User.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            {children}
+        </User.Provider>
+    )
+}
+
+export default UserContext;
+
+export function useUserContext() {
+    return useContext(User);
+}
