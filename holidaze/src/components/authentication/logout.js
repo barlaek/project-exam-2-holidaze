35332d507053538f835516b132d6b44f68e7c34a
Liridona <91525128+barlaek@users.noreply.github.com>
2023-05-15 18:45:05 +0200

    compiling in terminal, not in browser

--- holidaze/src/components/authentication/logout.js
+++ holidaze/src/components/authentication/logout.js
@@ -1,10 +1,17 @@
 // Removes the JWT from storage, invalidating the HTTP request
 // And changes the context state to false.
 
+import { useContext } from "react";
+import { UserContext } from "../../App";
+
 export function Logout() {
+    const [ currentUser, setCurrentUser ] = useContext(UserContext)
     function logOut() {
         localStorage.removeItem('accessToken');
         console.log(localStorage);
+        setCurrentUser(null)
+        console.log(currentUser);
+
     }
 
     return (
