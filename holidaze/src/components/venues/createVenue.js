8cf5a4f77708f02e1bdb7ad834c546105181e79e
Liridona <91525128+barlaek@users.noreply.github.com>
2023-05-21 13:23:13 +0200

    creating the schema object

--- holidaze/src/components/venues/createVenue.js
+++ holidaze/src/components/venues/createVenue.js
@@ -2,7 +2,44 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
-const schema = yup.object({}).required();
+const schema = yup.object({
+    created: yup.object().shape({
+            name: yup
+            .string()
+            .required('Please enter the name of your venue'),
+        description: yup
+            .string()
+            .required('Please provide description of the venue'),
+        media: yup.mixed()
+            .when('isArray', {
+                is: Array.isArray,
+                then: yup.array().of(yup.string().url()),
+                otherwise: yup.string().url(),
+            }),
+        price: yup
+            .number()
+            .required(),
+        ratings: yup
+            .number()
+            .default(0),
+        meta: yup
+            .object().shape({
+                wifi: yup.boolean().default(false),
+                parking: yup.boolean().default(false),
+                breakfast: yup.boolean().default(false),
+                pets: yup.boolean().default(false),
+            })
+    }),
+    location: yup.object().shape({
+        address: yup.string().default('Unkown'),
+        city: yup.string().default('Unkown'),
+        zip: yup.string().default('Unkown'),
+        country: yup.string().default('Unkown'),
+        continent: yup.string().default('Unkown'),
+        lat: yup.number().default(0),
+        lng: yup.number().default(0),
+    })
+}).required();
 
 export function CreateVenue() {
     return (
