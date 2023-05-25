3e5a39bbfc019fb18d41c069776a944b0e5db03d
Liridona <91525128+barlaek@users.noreply.github.com>
2023-05-25 19:50:29 +0200

    information about the venue

--- holidaze/src/components/venues/venuePop.js
+++ holidaze/src/components/venues/venuePop.js
@@ -16,8 +16,44 @@ export function VenuePopulation(props) {
                 <BookVenue />
             </div>
             <div>
-                <h2>{venueData.name}</h2>
-                <p>{venueData.description}</p>
+                <div>
+                    <h2>{venueData.name}</h2>
+                    <p>Price per night: {venueData.price}kr</p>
+                    <p>{venueData.description}</p>
+                </div>
+                <div>
+                    <h3>Information about the venue:</h3>
+                    <div>
+                        <h4>Location:</h4>
+                        <p>Addres: {venueData.location.address}</p>
+                        <p>City: {venueData.location.city}</p>
+                        <p>Country: {venueData.location.country}</p>
+                    </div>
+                    <div>
+                        <h4>Additional information</h4>
+                            {venueData.meta.breakfast ? (
+                            <p>Serves breakfast</p>
+                            ) : (
+                                <p>No breakfast</p>
+                            )}
+                            {venueData.meta.parking ? (
+                                <p>Has parking</p>
+                            ) : (
+                                <p>No parking</p>
+                            )}
+                            {venueData.meta.pets ? (
+                                <p>Pet friendly</p>
+                            ) : (
+                                <p>No pets allowed</p>
+                            )}
+                            {venueData.meta.wifi ? (
+                                <p>Has wifi</p>
+                            ) : (
+                                <p>No wifi</p>
+                            )}
+                    </div>
+                    <p>This post was last updated: {venueData.updated}</p>
+                </div>
             </div>
         </div>
     )
