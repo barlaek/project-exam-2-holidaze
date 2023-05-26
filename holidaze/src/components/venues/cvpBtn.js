907606a367e2e83f764e9ee23b8002cf9f0819c7
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-05-26 09:51:45 +0200

    debugging conditional rendering of cvp

--- holidaze/src/components/venues/cvpBtn.js
+++ holidaze/src/components/venues/cvpBtn.js
@@ -1,18 +1,21 @@
+import { Children } from "react";
 import { Link } from "react-router-dom"
 
 export function CvpBtn() {
 
     const viewButton = () => {
-        const localData = localStorage.getItem('userBody');
-        const venueManager = localData.venueManager;
-        if(venueManager) {
-            return <Link to="/venues/createvenue"><button>Create venue</button></Link>
+        const localData = JSON.parse(localStorage.getItem("userBody"));
+        console.log(localData)
+        if(localData.venueManager === true) {
+            return <Link to="/venues/createvenue/"><button>Create venue</button></Link>
         } else {
-            return <div></div>
+            return <p>butt</p>
         }
+    
     }
 
     return (
-        <div value={viewButton}></div>
+        <div>{viewButton}
+        </div>
     )
 }
