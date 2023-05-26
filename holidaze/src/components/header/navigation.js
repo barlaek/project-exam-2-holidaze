a55bf06f083d677c69a5620db0ff855a9fe7b561
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-05-26 10:51:34 +0200

    struggles with properties of null

--- holidaze/src/components/header/navigation.js
+++ holidaze/src/components/header/navigation.js
@@ -3,6 +3,7 @@ import { LogBtn } from "./logBtn";
 import { SearchBar } from "./searchbar";
 import { ProfileBtn } from "../profilePortal/profileNavBTN";
 import { CvpBtn } from "../venues/cvpBtn";
+import { Link } from "react-router-dom";
 
 export function Navigation() {
 
--- holidaze/src/components/venues/cvpBtn.js
+++ holidaze/src/components/venues/cvpBtn.js
@@ -1,21 +1,14 @@
-import { Children } from "react";
-import { Link } from "react-router-dom"
-
 export function CvpBtn() {
-
-    const viewButton = () => {
-        const localData = JSON.parse(localStorage.getItem("userBody"));
-        console.log(localData)
-        if(localData.venueManager === true) {
-            return <Link to="/venues/createvenue/"><button>Create venue</button></Link>
-        } else {
-            return <p>butt</p>
-        }
+    const localData = JSON.parse(localStorage.getItem("userBody"))
     
-    }
 
     return (
-        <div>{viewButton}
+        <div>
+            {localData ? (
+                <button>Create venue</button>
+            ) : (
+                <p>Butt</p>
+            )}
         </div>
     )
 }
